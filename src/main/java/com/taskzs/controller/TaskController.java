af3d7005ebbcac1814755d7564fba5a311457119
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-12-02 22:55:02 -0300

    feat: Adiciona endpoints na classe controller e a lógica na classe service

--- src/main/java/com/taskzs/controller/TaskController.java
+++ src/main/java/com/taskzs/controller/TaskController.java
@@ -1,9 +1,17 @@
 package com.taskzs.controller;
 
+import com.taskzs.controller.mapper.TaskMapper;
+import com.taskzs.controller.request.TaskRequest;
+import com.taskzs.controller.response.TaskResponse;
+import com.taskzs.entity.Task;
 import com.taskzs.service.TaskService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +19,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class TaskController {
 
     private final TaskService service;
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> findAll() {
+        return ResponseEntity.ok(service.findAll().stream()
+                .map(TaskMapper::toTaskResponse).toList());
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<TaskResponse> createTask(@RequestBody TaskRequest request) {
+        Task saved = service.saveTask(TaskMapper.toTask(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(TaskMapper.toTaskResponse(saved));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable String id) {
+        Optional<Task> optionalTask = service.findById(id);
+        if(optionalTask.isPresent()) {
+            service.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return  ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    }
 }
--- src/main/java/com/taskzs/service/TaskService.java
+++ src/main/java/com/taskzs/service/TaskService.java
@@ -1,12 +1,55 @@
 package com.taskzs.service;
 
+import com.taskzs.entity.Task;
 import com.taskzs.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class TaskService {
-
+    private final LocalDate date = LocalDate.now();
     private final TaskRepository repository;
+
+    public List<Task> findAll() {
+        return repository.findAll();
+    }
+
+    public Task saveTask(Task task) {
+        if(!task.getDeadline().isBefore(date)) {
+            if(findMaxOrder().isPresent()) {
+                Integer i = findMaxOrder().get();
+                task.setOrder(i+1);
+                return repository.save(task);
+            }
+
+            task.setOrder(1);
+            return repository.save(task);
+        }
+
+
+        return null;
+
+    }
+
+    private Optional<Integer> findMaxOrder() {
+        return repository.findAll().stream()
+                .map(Task::getOrder)
+                .max(Comparator.naturalOrder());
+    }
+
+    public void deleteById(String id) {
+
+       repository.deleteById(id);
+
+    }
+
+    public Optional<Task> findById(String id) {
+       return repository.findById(id);
+    }
 }
