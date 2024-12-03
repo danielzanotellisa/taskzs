ec4ac31e3f7b6805616726bb4b3dbb5c0d73639e
Daniel Zanotelli <danielzanotelli12@gmail.com>
2024-12-02 21:55:59 -0300

    fix: Modifica o arquivo de migration e minha entity para adicionar uma nova coluna(ainda não foi iniciado)

--- src/main/java/com/taskzs/entity/Task.java
+++ src/main/java/com/taskzs/entity/Task.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
 
@@ -25,7 +26,10 @@ public class Task {
     private String name;
 
     @Column(nullable = false)
-    private Date deadline;
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDate deadline;
 
     @CreationTimestamp
     @Column(name = "created_at")
--- src/main/resources/db/migration/V1__create_task_table.sql
+++ src/main/resources/db/migration/V1__create_task_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE tasks(
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name varchar(100) NOT NULL,
+    description varchar(255) not null,
     deadline date NOT NULL,
     created_at timestamp,
     order integer
